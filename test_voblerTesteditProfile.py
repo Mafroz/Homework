ed4339526c4d765e5450376f9ecd781012591594
Mafroz <maxim.zasetski@gmail.com>
2020-01-16 21:48:49 +0300

    Initial commit

---
+++ test_voblerTesteditProfile.py
@@ -0,0 +1,35 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class TestVoblerTesteditProfile():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_voblerTesteditProfile(self):
+    self.driver.get("http://voblery.com.ua/")
+    self.driver.set_window_size(1693, 1050)
+    self.driver.find_element(By.ID, "loglink2").click()
+    self.driver.find_element(By.ID, "email").click()
+    self.driver.find_element(By.ID, "email").send_keys("maxim.zasetski@gmail.com")
+    self.driver.find_element(By.ID, "passLabel").click()
+    self.driver.find_element(By.ID, "pass").send_keys("Ckjybr13")
+    self.driver.find_element(By.CSS_SELECTOR, "#logform2 .button").click()
+    self.driver.find_element(By.LINK_TEXT, "Новинки").click()
+    self.driver.find_element(By.ID, "loglink4").click()
+    self.driver.find_element(By.LINK_TEXT, "Мой профиль").click()
+    self.driver.find_element(By.NAME, "name").click()
+    self.driver.find_element(By.NAME, "name").send_keys("Hoverclass")
+    self.driver.find_element(By.LINK_TEXT, "Обновить аккаунт").click()
+    self.driver.find_element(By.ID, "loglink5").click()
+  
